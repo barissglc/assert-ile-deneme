f753282f3203b24e0757887ca4aa7dc1fd8765cd
Barış Güleç <87657907+barissglc@users.noreply.github.com>
2023-02-21 01:38:19 +0300

    test
    test

---
+++ unit.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <assert.h>
+#include "test.c"
+
+int main() {
+    int topla_sonuc = topla(2, 4);
+    int cikar_sonuc = cikar(5, 3);
+    int carp_sonuc = carp(2, 3);
+    double bol_sonuc1 = bol(6.0, 3.0);
+    double bol_sonuc2 = bol(6.0, 0.0);
+
+    int success = 1;
+
+    // Toplama testi
+    if (topla_sonuc != 6) {
+        success = 0;
+        printf("Toplama testi basarisiz.\n");
+    }
+
+    // Çıkarma testi
+    if (cikar_sonuc != 2) {
+        success = 0;
+        printf("Cikarma testi basarisiz.\n");
+    }
+
+    // Çarpma testi
+    if (carp_sonuc != 6) {
+        success = 0;
+        printf("Carpma testi basarisiz.\n");
+    }
+
+    // Bölme testi
+    if (bol_sonuc1 != 2.0) {
+        success = 0;
+        printf("Bolme testi 1 basarisiz.\n");
+    }
+    if (bol_sonuc2 != 0.0) {
+        success = 0;
+        printf("Bolme testi 2 basarisiz.\n");
+    }
+
+    if (success) {
+        printf("Tum testler basarili.\n");
+    }
+
+    return 0;
+}
